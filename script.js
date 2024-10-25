76285b6cc7db1a9a27465d63bf869959d56c03ba
Colin Toomey <86315504+crtoomey@users.noreply.github.com>
2024-10-25 13:32:41 -0400

    Modify HTML, CSS, and JS
    Modified index.html to add the UI for the calculator as well as header and footers.
    Modified style.css to add styling for the entire page. Made the calculator look nice.
    Modified script.js but the function to get the first number, second number, and the operator is not finished and is going to change.

--- script.js
+++ script.js
@@ -27,3 +27,18 @@ console.log(subtract(1, 2))
 console.log(multiply(1, 2))
 
 console.log(divide(1, 2))
+
+// Operate function takes 3 strings, converts the number strings into ints and calls a math function from above
+function operate(firstNum, secondNum, operator) {
+    Number(firstNum);
+    Number(secondNum);
+    if (operator === '+') {
+        add(firstNum, secondNum);
+    } else if (operator === '-') {
+        subtract(firstNum, secondNum);
+    } else if (operator === '*') {
+        multiply(firstNum, secondNum);
+    } else if (operator === '/') {
+        divide(firstNum, secondNum);
+    }
+}
