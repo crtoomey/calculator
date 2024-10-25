ce6825d92305ce4fb311f33497ed2f120a53634b
Colin Toomey <86315504+crtoomey@users.noreply.github.com>
2024-10-25 16:10:54 -0400

    Modify HTML, CSS, and JS files
    Modified script.js to add functionality so that you can put in two numbers and perform an operation on them. Does not work for more than 2 numbers right now.
    Modified other files to support the changes made to script.js.

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-let firstNumber = '';
-let secondNumber = '';
-let operator = '';
+let firstNumberElement = document.querySelector('.first-number');
+let secondNumberElement = document.querySelector('.second-number');
+let operatorElement = document.querySelector('.operator');
+let result = document.querySelector('.result');
+let firstNumber = firstNumberElement.innerText;
+let secondNumber = secondNumberElement.innerText;
+let operator = operatorElement.innerText;
+let operatorButtonClicked = false;
+// turn the strings of numbers into number types
+// firstNumber = Number(firstNumber);
+// secondNumber = Number(secondNumber);
+
+
+console.log(firstNumber);
+console.log(secondNumber);
+console.log(operator);
 
 // Math functions
 function add(a, b) {
@@ -19,26 +32,58 @@ function divide(a, b) {
     return a / b;
 }
 
-// Testing math functions
-console.log(add(1, 2))
+// Functions to track numbers and the operator (this is terrible code I am sorry but this is how I got it to work. Might fix later)
+function operatorHasBeenClicked(op) {
+    console.log(op);
+    return operatorButtonClicked = true, operatorElement.innerText = op, operator = op;
+}
 
-console.log(subtract(1, 2))
+function updateFirstNum(num) {
+    return firstNumber = num;
+}
 
-console.log(multiply(1, 2))
+function updateSecondNum(num) {
+    return secondNumber = num;
+}
 
-console.log(divide(1, 2))
+
+// function to get the input of numbers
+function getInput(input) {
+    let firstInput = 0;
+    let secondInput = 0;
+    if (operatorButtonClicked === false) {
+        firstInput = input;
+        firstNumberElement.innerText += firstInput;
+        updateFirstNum(firstNumberElement.innerText);
+        console.log(firstNumber);
+        return firstNumberElement.innerText;
+    } else if (operatorButtonClicked === true) {
+        secondInput = input;
+        secondNumberElement.innerText += secondInput;
+        updateSecondNum(secondNumberElement.innerText);
+        console.log(secondNumber);
+        return secondNumberElement.innerText;
+    }
+}
 
 // Operate function takes 3 strings, converts the number strings into ints and calls a math function from above
 function operate(firstNum, secondNum, operator) {
+    console.log(firstNum, secondNum, operator);
+    let output = '';
     Number(firstNum);
     Number(secondNum);
     if (operator === '+') {
-        add(firstNum, secondNum);
+        output = add(firstNum, secondNum);
+        output.toString();
     } else if (operator === '-') {
-        subtract(firstNum, secondNum);
+        output = subtract(firstNum, secondNum);
+        output.toString();
     } else if (operator === '*') {
-        multiply(firstNum, secondNum);
+        output = multiply(firstNum, secondNum);
+        output.toString();
     } else if (operator === '/') {
-        divide(firstNum, secondNum);
+        output = divide(firstNum, secondNum);
+        output.toString();
     }
+    return result.innerText = output;
 }
