a072ec284d7595cca9ee43acfe97bab445dea5cf
Colin Toomey <86315504+crtoomey@users.noreply.github.com>
2024-10-25 11:37:51 -0400

    Modify HTML and JS
    Modified index.html to link the JavaScript file.
    Added 4 math functions to handle addition, subtraction, multiplication, and division. Tested these in the console.

---
+++ script.js
@@ -0,0 +1,29 @@
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+
+// Math functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// Testing math functions
+console.log(add(1, 2))
+
+console.log(subtract(1, 2))
+
+console.log(multiply(1, 2))
+
+console.log(divide(1, 2))
