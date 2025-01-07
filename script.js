ed328121d6c490711c1446331e56ac77b693dac6
Colin Toomey <86315504+crtoomey@users.noreply.github.com>
2025-01-07 12:13:49 -0500

    Modify HTML and JS
    Modified the index.html file slightly to remove the default number of 0. I will come back to this and either reimplement it or do it in JS.
    Modified the script.js file to handle operations better, chain operations, and clean up some of my code.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-let firstNum = 0;
-let secondNum = 0;
-let operator = '';
+let firstNum = null;
+let secondNum = null;
+let operator = null;
+let currentInput = '';
 let displayElement = document.querySelector('.num-display');
 let defaultNum = document.getElementById('default-num');
-let operatorHasBeenClicked = false;
 
 // Math functions
 function add(a, b) {
@@ -22,63 +22,63 @@ function divide(a, b) {
 
 // Function to handle getting a number and updating the display
 function updateDisplay(input) {
-
-    defaultNum.style.display = "none";
-    displayElement.innerText += input;
-
+    currentInput += input;
+    displayElement.innerText = currentInput;
 
     return displayElement.innerText;
 }
 
-// Function to get a number
+// Function to get numbers and operator
 function getOperator(input) {
-    operator = input;
-    console.log(operator);
-    if (operatorHasBeenClicked === false) {
-        firstNum = displayElement.innerText;
-        Number(firstNum);
-        displayElement.innerText = '';
-        console.log("first num is " + firstNum);
-        operatorHasBeenClicked = true;
-    } else if (operatorHasBeenClicked === true) {
-        secondNum = displayElement.innerText;
-        Number(secondNum);
-        console.log("second num is " + secondNum);
-        operatorHasBeenClicked = false;
-        operate(firstNum, secondNum, operator)
+
+    if (currentInput !== "") {
+        if (firstNum === null) {
+            firstNum = Number(currentInput);
+            currentInput = '';
+        } else if (operator) {
+            secondNum = Number(currentInput);
+            firstNum = operate(firstNum, secondNum, operator);
+        }
     }
+
+    operator = input;
+    currentInput = '';
 
 }
 
 
-
+// func to perform operation on equals click
+function performOperation() {
+    if (currentInput !== '' && operator) {
+        secondNum = Number(currentInput);
+        let result = operate(firstNum, secondNum, operator);
+        displayElement.innerText = result;
+        firstNum = result;
+        operator = null;
+        currentInput = '';
+    }
+}
 
 // function to handle operations
 function operate(firstNumber, secondNumber, operator) {
-    Number(firstNumber);
-    Number(secondNumber);
-    let result = 0;
-    displayElement.innerText = '';
     if (operator === '+') {
         result = add(firstNumber, secondNumber);
-        displayElement.innerText = result;
     } else if (operator === '-') {
         result = subtract(firstNumber, secondNumber);
-        displayElement.innerText = result;
     } else if (operator === '*') {
         result = multiply(firstNumber, secondNumber);
-        displayElement.innerText = result;
     } else if (operator === '/') {
         result = divide(firstNumber, secondNumber);
-        displayElement.innerText = result;
     } else {
         console.log('ERROR');
     }
+    displayElement.innerText = result;
+    return result;
 }
 
 
 // Testing math functions
-console.log(add(1, 2))
-console.log(subtract(1, 2))
-console.log(multiply(1, 2))
-console.log(divide(1, 2))
+// console.log(add(1, 2))
+// console.log(subtract(1, 2))
+// console.log(multiply(1, 2))
+// console.log(divide(1, 2))
